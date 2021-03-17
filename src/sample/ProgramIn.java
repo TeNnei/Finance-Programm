a147e01164b002eece16128353c60c13ece0d6f8
TeNnei <wled3882@gmail.com>
2021-03-17 13:07:18 +0500

    Update ProgramIn.java

--- src/sample/ProgramIn.java
+++ src/sample/ProgramIn.java
@@ -12,18 +12,27 @@ import java.sql.Date;
 
 
 public class ProgramIn {
-    @FXML private TextField contract_number;
-    @FXML private TextField debit;
-    @FXML private TextField kredit;
-    @FXML private TextField comments;
-    @FXML private TextField som;
-    @FXML private Button contin;
-    @FXML private TextField contract;
-    @FXML private DatePicker date;
-    @FXML private TextField usd;
+    @FXML
+    private TextField contract_number;
+    @FXML
+    private TextField debit;
+    @FXML
+    private TextField kredit;
+    @FXML
+    private TextField comments;
+    @FXML
+    private TextField som;
+    @FXML
+    private Button contin;
+    @FXML
+    private TextField contract;
+    @FXML
+    private DatePicker date;
+    @FXML
+    private TextField usd;
 
     @FXML
-    void initialize (){
+    void initialize() {
         contin.setOnAction(actionEvent -> {
             try {
                 registOperation();
@@ -38,10 +47,12 @@ public class ProgramIn {
             updateConsolidInf1();
             updateConsoliddebit();
             updateConsolidkredit();
+            updateConsolidUpdate();
         });
 
     }
-    public void registOperation () throws ParseException {
+
+    public void registOperation() throws ParseException {
         DatabaseHandler db = new DatabaseHandler();
         String contract_number1 = contract_number.getText();
         String contract1 = contract.getText();
@@ -54,7 +65,8 @@ public class ProgramIn {
         ProgramData inf = new ProgramData(contract_number1, contract1, debit1, credit1, date1, comment, som1, usd1);
         db.writeInProgram(inf);
     }
-    public void registConsolidatedInf(){
+
+    public void registConsolidatedInf() {
         DatabaseHandler db = new DatabaseHandler();
         int code1 = Integer.parseInt(debit.getText());
         String category1 = " ";
@@ -74,7 +86,8 @@ public class ProgramIn {
                 saldo_in_som1, kredit1, saldo_out_som1, difference1, debit_usd1, saldo_in_usd1, kredit_usd1, saldo_out_usd1, difference_usd1);
         db.writeInProgram2(input);
     }
-    public void checkCodeIn (int code){
+
+    public void checkCodeIn(int code) {
         DatabaseHandler read = new DatabaseHandler();
         ConsolidInfin pr = new ConsolidInfin();
         pr.setCode(code);
@@ -87,46 +100,47 @@ public class ProgramIn {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        if(fg >= 1)
-        {
+        if (fg >= 1) {
             System.out.println("Success!");
-        }
-        else{
+        } else {
             registConsolidatedInf();
         }
     }
 
-    public void updateConsolidInf(){
+    public void updateConsolidInf() {
         DatabaseHandler db = new DatabaseHandler();
         int debit2 = Integer.parseInt(debit.getText());
         ConsolidInfin inf = new ConsolidInfin(debit2);
         db.consolidUpdate(inf);
     }
 
-    public void updateConsolidInf1(){
+    public void updateConsolidInf1() {
         DatabaseHandler db = new DatabaseHandler();
         int debit2 = Integer.parseInt(kredit.getText());
         ConsolidInfin inf = new ConsolidInfin(debit2);
         db.consolidUpdate(inf);
     }
 
-    public void updateConsoliddebit(){
+    public void updateConsoliddebit() {
         DatabaseHandler db = new DatabaseHandler();
         int debit2 = Integer.parseInt(debit.getText());
         ConsolidInfin inf = new ConsolidInfin(debit2);
         db.consolidUpdatedebit(inf);
     }
 
-    public void updateConsolidkredit(){
+    public void updateConsolidkredit() {
         DatabaseHandler db = new DatabaseHandler();
         int debit2 = Integer.parseInt(kredit.getText());
         ConsolidInfin inf = new ConsolidInfin(debit2);
         db.consolidUpdatekredit(inf);
     }
+
+    public void updateConsolidUpdate() {
+        DatabaseHandler db = new DatabaseHandler();
+        int debit2 = Integer.parseInt(debit.getText());
+        ConsolidInfin inf = new ConsolidInfin(debit2);
+        db.consolidUpdateOut(inf);
+    }
 }
-
-            int codeIn = Integer.parseInt(debit.getText());
-            checkCodeIn(codeIn);
-
 
 
